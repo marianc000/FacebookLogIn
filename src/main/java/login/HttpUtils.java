7966d4cfee2ffaf8fd89688c9a8ca4c5c1f1784a
Marian <dendrofit@mail.com>
2018-02-06 23:05:03 +0100

    gmail and facebook logins  done

--- src/main/java/login/HttpUtils.java
+++ src/main/java/login/HttpUtils.java
@@ -6,12 +6,7 @@
 package login;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import javax.json.Json;
 import javax.json.JsonObject;
